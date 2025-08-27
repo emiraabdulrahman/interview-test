1b5b7577973d076dcfde1375d1d2c8c3f75b3082
emiraabdulrahman <137463639+emiraabdulrahman@users.noreply.github.com>
2025-08-27 23:59:23 +0800

    update
    remove unused files

--- app/api/movies/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getMovieDetails } from '@/library/tmdb';
-
-export async function GET(_req: Request, { params }: { params: { id: string } }) {
-  const data = await getMovieDetails(Number(params.id))
-  return NextResponse.json(data)
-}
